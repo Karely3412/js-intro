2fe60ecd86dc95e0a399f54e6f411b1c76e8c4db
Karely3412 <karely@devpipeline.com>
2024-07-08 13:59:27 -0600

    created & partially finished loops challenge

--- scripts/loopsChallenge.js
+++ scripts/loopsChallenge.js
@@ -1,3 +1,6 @@
+// Challenge 1
+// - - - - - - - - - - - - - - -
+
 function byTwoCount(anArray) {
   const returnArray = [];
   anArray.forEach((val, idx) => {
@@ -9,3 +12,22 @@ function byTwoCount(anArray) {
 }
 
 console.log(byTwoCount([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// Challenge 2
+// - - - - - - - - - - - - - - -
+
+// removeByIndex[‘a', 'b', 'c', 'd', 'f', 'f', 'e', 'f'], [1, 5]) => ['b', 'f']]
+
+const removeByIndex = (myArray, idx) => {
+  const newArray = [];
+  myArray.forEach((val, idx) => {
+    if (val == idx[0]) {
+      newArray.push(val);
+    } else if (val == idx[1]) {
+      newArray.push(val);
+    }
+  });
+  return newArray;
+};
+
+console.log(removeByIndex(["a", "b", "c", "d", "f", "f", "e", "f"], [1, 5]));
