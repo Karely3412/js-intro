669ebd1ef9717ec9ccff5312c4755f51dc5af1b8
Karely3412 <karely@devpipeline.com>
2024-07-07 22:08:39 -0600

    created greater num func

--- scripts/conditionalChallenges.js
+++ scripts/conditionalChallenges.js
@@ -1,3 +1,6 @@
+// Challenge 1
+// - - - - - - - - - - - -
+
 const outterObject = {
   firstObject: { id: 1, name: "Karely" },
   secObject: { address: "123 fake st", Phone: "555-555-5555" },
@@ -6,6 +9,18 @@ const outterObject = {
 console.log(outterObject.firstObject);
 console.log(outterObject["secObject"]);
 console.log(outterObject["firstObject"], outterObject.secObject);
+
+// Challenge 2
+// - - - - - - - - - - - -
+
+const largestNum = (numOne, numTwo) => {
+  return numOne > numTwo ? numOne : numTwo > numOne ? numTwo : "they are equal";
+};
+
+console.log(largestNum(108, 21));
+
+// Challenge 3
+// - - - - - - - - - - - -
 
 sentence = "I am a sentence";
 
@@ -18,3 +33,6 @@ const firstCapitalLet = (sentence) => {
 };
 
 firstCapitalLet(sentence);
+
+// Challenge 4
+// - - - - - - - - - - - -
