c1624df2d9b5d5e849a277cf339da6530f8fc292
Karely3412 <karely@devpipeline.com>
2024-07-14 13:58:34 -0600

    updated variable names

--- scripts/jsonObjectReturn.js
+++ scripts/jsonObjectReturn.js
@@ -15,12 +15,13 @@ class User {
   }
 }
 
-johnnyBravo = new User(2, "Johnny Bravo", "test@tester.com", "4321");
-mickeyMouse = new User(3, "Mickey Mouse", "test@testing.com", "2134");
+userOne = new User(2, "Johnny Bravo", "test@tester.com", "4321");
+userTwo = new User(3, "Mickey Mouse", "test@testing.com", "2134");
 
-console.log(`Hello ${johnnyBravo.name}your email is ${johnnyBravo.email}`);
-console.log(johnnyBravo);
-console.log(mickeyMouse);
+console.log(`Hello ${userOne.name}your email is ${userOne.email}`);
+console.log(`Hello ${userTwo.name}your email is ${userTwo.email}`);
+console.log(userOne);
+console.log(userTwo);
 
 // Challenge 2
 // - - - - - - - - - -
